7013ce86367db88df94f7cfc22b60e01dcc262d3
amanmulani09 <mulaniaman0504@gmail.com>
2026-02-09 22:41:52 +0530

    fix: add cache using redis

--- sync_vs_async/main.py
+++ sync_vs_async/main.py
@@ -1,13 +1,28 @@
 import time
+import asyncio
 
-def fetch_data():
-    time.sleep(2)  # pretend API call
-    return "Data fetched"
+# def fetch_data():
+#     time.sleep(2)  # pretend API call
+#     return "Data fetched"
 
-print("Start") # Print Start
-result = fetch_data() # waits for 2 second 
-print(result) # print result 
-print("End") # print End 
+# print("Start") # Print Start
+# result = fetch_data() # waits for 2 second 
+# print(result) # print result 
+# print("End") # print End 
 
 
 # synchnous (line by line, waits until the current function execution ends(wait,db,i/o))
+
+
+
+async def fetch_data():
+    await asyncio.sleep(2)  # non-blocking sleep
+    return "Data fetched"
+
+async def main():
+    print("Start")
+    result = await fetch_data()
+    print(result)
+    print("End")
+
+asyncio.run(main())
