13c55ba5b0776088f70b73c409627bb757252ce3
amanmulani09 <mulaniaman0504@gmail.com>
2026-02-19 23:32:30 +0530

    feat: add test cases with pytest

--- fastapi_pytest/test_main.py
+++ fastapi_pytest/test_main.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
+from fastapi.testclient import TestClient # import TestClient 
+from main import app # Import app from original 
 
-app = FastAPI()
+client = TestClient(app)
 
-@app.get('/')
-def read_root():
-    return {"message":"Hello World"}
+
+# create test cases 
+
+def test_create_task():
+    response = client.post(
+        "/task",
+        json={"id":"1","title":"Test Title","completed":False}
+    )
+    
+    assert response.status_code == 200 # assert means equal to or to check with 
+    assert response.json()['title'] == "Test Title"
+    
+    
+def test_get_tasks():
+    response = client.get('/tasks')
+    
+    assert response.status_code == 200
+    assert isinstance(response.json(),list)
+    
+
+def test_get_task_by_id():
+    response = client.get('/task/1')
+    
+    assert response.status_code == 200
+    assert response.json()['id'] == 1
+    
+    
+def test_task_not_found():
+    response = client.get('task/999')
+    
+    assert response.status_code == 404
+    print(response.json())
+    assert response.json()['detail'] == "Task Not Found"
