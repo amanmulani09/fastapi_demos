f7297b15c3256520eccb1e5c5df1e027b6182121
amanmulani09 <mulaniaman0504@gmail.com>
2026-03-13 09:38:28 +0530

    feat: pydantic response format

--- fastapi_pydantic_validation/main.py
+++ fastapi_pydantic_validation/main.py
@@ -4,22 +4,25 @@ from typing import Optional
 
 app = FastAPI()
 
-class CreateUser(BaseModel):
+class User(BaseModel):
     name : str = Field(...,min_length=3,max_length=50)
     age : int = Field(..., gt=18,lt=60)
     email: EmailStr
-    
-    # custom validation 
-    
+class UserCreateRequest(User):
+    pass
+
+class CreateUserResponse(BaseModel):
+    msg: str
+    data:User    
    
-class CreateProduct(BaseModel):
+class UserCreateResponse(BaseModel):
     name : str = Field(min_length=3)
     price: float = Field(gt=0, lt=100000)   
     stock: int = Field(gt=0,)
     category:Optional[str] = None
 
-@app.post('/user')
-def create_user(user:CreateUser): #CreateUser is the schema of the request body
+@app.post('/user',response_model=UserCreateResponse)
+def create_user(user:UserCreateRequest): #CreateUser is the schema of the request body
     return {
         "msg":"user created successfully",
         "data":user
