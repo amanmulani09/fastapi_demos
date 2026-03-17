623e94f108b12d1198616490f0e1898e6eff25c1
amanmulani09 <mulaniaman0504@gmail.com>
2026-03-17 22:38:48 +0530

    auth dependency example

--- fastapi_dependency_injection/main.py
+++ fastapi_dependency_injection/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from typing import Annotated
 app = FastAPI()
 
@@ -37,3 +37,31 @@ email_service_deps = Annotated[EmailService, Depends(get_email_service)]
 def send_email(receipent:str,email:str,email_service:email_service_deps):
     
     email_service.send_email(receipent,email)
+    
+    
+class Authservice:
+    def validate_token(_self,token:str):
+        
+        if(token == "valid-token"):
+            return True
+        else: 
+            raise HTTPException(status_code=401, detail="unauthorized user")
+        
+        
+# create a fn to create auth service instance
+
+def get_auth_service():
+    return Authservice()
+
+# create a auth service dependecy 
+
+auth_service_deps= Annotated[Authservice, Depends(get_auth_service)]
+
+
+@app.post('/generate-report')
+def generate_report(token:str,auth_service:auth_service_deps):
+    
+    is_authenticated_user = auth_service.validate_token(token)
+    
+    if is_authenticated_user:
+        return {"billo bagge billiya da ki karegi..."}
