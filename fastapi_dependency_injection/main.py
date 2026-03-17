dab23517f883632a96fe2a34886424f9d47a4948
amanmulani09 <mulaniaman0504@gmail.com>
2026-03-17 22:31:52 +0530

    implement dependency example

--- fastapi_dependency_injection/main.py
+++ fastapi_dependency_injection/main.py
@@ -18,3 +18,22 @@ def log_message(message:str, logger:logger_deps):
     logger.log(message)
     
     return message
+
+
+class EmailService:
+    def send_email(receipent:str,email:str):
+        print(f"sending email : {email} to {receipent}")
+        
+        
+# a fn to get the EmailService instance
+
+def get_email_service():
+    return EmailService()
+
+# a dependecy of email service 
+
+email_service_deps = Annotated[EmailService, Depends(get_email_service)]
+
+def send_email(receipent:str,email:str,email_service:email_service_deps):
+    
+    email_service.send_email(receipent,email)
